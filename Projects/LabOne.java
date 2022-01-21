b01757c1e13d20c9e5e3e7fc8f504a4a28a5c911
Aushveen <53095243+Aushveen@users.noreply.github.com>
2022-01-21 16:08:53 -0500

    Add files via upload

---
+++ Projects/LabOne.java
@@ -0,0 +1,21 @@
+import java.math.*;
+
+
+public class LabOne {
+    public static void main(String[] args){
+        BigDecimal a = new BigDecimal("1");
+        BigDecimal b = new BigDecimal("2");
+        BigDecimal c = new BigDecimal("3");
+        BigDecimal d = new BigDecimal("4");
+        BigDecimal e = new BigDecimal("5");
+
+        System.out.println("3 * (1 + 2) = " + c.multiply(a.add(b)));
+        System.out.println("1 + 2 = " + a.add(b));
+        System.out.println("2 * 3 = " + b.multiply(c));
+        System.out.println("(1 + (2 + (3 + 4))) = " + a.add(b.add(c.add(d))));
+        System.out.println("1 + 2 + 3 + 4 = " + a.add(b).add(c).add(d));
+        System.out.println("2 * 3 + 4 * 5 = " + b.multiply(c).add(d.multiply(e)));
+
+    }
+
+}
