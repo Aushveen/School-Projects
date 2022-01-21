eca0d1c212bd9ee4d21d78731a01be2855615744
Aushveen <53095243+Aushveen@users.noreply.github.com>
2022-01-21 15:58:55 -0500

    Add files via upload

--- Homework/HomeworkOne.java
+++ Homework/HomeworkOne.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 
 public class HomeworkOne {
 
+    private static BigInteger[] series = new BigInteger[10000000];
+
     public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
-	System.out.print("What fibonacci number do you want to know: ");
-	int n = in.nextInt();
+        Scanner in = new Scanner(System.in);
+        System.out.print("What fibonacci number do you want to know: ");
+        int n = in.nextInt();
         System.out.println("Fibonacci of " + n + " = " + fib(n));
-
-
     }
+
     public static BigInteger fib(int n){
-        if (n == 0 || n == 1){
+        if(n < 2) {
             return BigInteger.ONE;
         }
-        return fib(n-1).add(fib(n-2));
+
+        if(series[n] != null) {
+            return series[n];
+        }
+
+        series[n] = fib(n - 1).add(fib(n - 2));
+        return series[n];
     }
 }
