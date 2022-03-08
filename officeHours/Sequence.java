adde931e0f33edd6adfe8c1d1d2ddbd41cf60775
c212 <40395648+c212@users.noreply.github.com>
2022-03-07 20:12:21 -0500

    add sum to Sequence

--- officeHours/Sequence.java
+++ officeHours/Sequence.java
@@ -1,91 +1,41 @@
 import java.util.ArrayList;
 
-public class Sequence extends ArrayList<Integer> {
+public class Sequence {
     private int[] values;
     public Sequence(int size) { values = new int[size]; }
     public void set(int i, int n) { values[i] = n; }
     public int obtain(int i) { return values[i]; }
     public int size() { return values.length; }
+    public String toString() {
+        return java.util.Arrays.toString( values );
+    }
     public static void main(String[] args){
-        Sequence one = new Sequence(10);
-        Sequence two = new Sequence(10);
-        one.set(0,84);
-        one.set(1,10);
-        one.set(2, 69);
-        one.set(3, 12);
-        one.set(4, 22);
-        one.set(5, 10);
-        one.set(6, 4000000);
-        one.set(7, 12);
-        one.set(8, 22);
-        one.set(9, 88);
-        two.set(0,84);
-        two.set(1,10);
-        two.set(2, 69);
-        two.set(3, 12);
-        two.set(4, 22);
-        two.set(5, 10);
-        two.set(6, 4000000);
-        two.set(7, 12);
-        two.set(8, 22);
-        two.set(9, 88);
+        Sequence one = new Sequence(3);
+        Sequence two = new Sequence(4);
+        one.set(0, -1);
+        one.set(1, 6);
+        one.set(2, 3);
+        two.set(0, 2);
+        two.set(1, 5);
+        two.set(2, 1);
+        two.set(3, 7);
         System.out.println(one);
         System.out.println(two);
+        System.out.println("One plus two is: " + one.sum(two));
         /**
         System.out.println("One is equal to two: " + one.equals(two));
         System.out.println("One has the same values has two: " + one.SameValues(two));
-        System.out.println("One plus two is: " + one.sum(two));
          **/
 
     }
-
-    public boolean equals(Sequence other){
-        boolean result = true;
-        if (other.size() != this.size())
-            return false;
-        for(int i = 0; i < other.size(); i++){
-            if(other.get(i) != this.get(i))
-                result = false;
+    public Sequence sum(Sequence other) {
+        Sequence result = new Sequence( Math.max( this.size(), other.size() ) );
+        for (int i = 0; i < result.size(); i++) {
+            int a, b; 
+            if (i < this.size()) a = values[i]; else a = 0; 
+            if (i < other.size()) b = other.obtain(i); else b = 0; 
+            result.set(i, a + b);             
         }
         return result;
-    }
-
-    //E7.12
-    public boolean SameValues(Sequence other){
-        boolean result = true;
-        int compare;
-        for (int i = 0; i < other.size(); i++){
-            compare = this.get(i);
-            for (int j = 0; j < other.size(); j++){
-                if (this.get(j) == compare) {
-                    result = true;
-                    break;
-                }
-                else
-                    result = false;
-            }
-            if (result == false)
-                return result;
-        }
-        return result;
-    }
-
-    //e7.14
-    public Sequence sum(Sequence other){
-        Sequence add = new Sequence(0);
-        for (int i =0; i < Math.max(this.size(), other.size()); i++){
-            int b, c;
-            if(i < this.size())
-                b = this.get(i);
-            else
-                b = 0;
-            if (i < other.size())
-                c = other.get(i);
-            else
-                c = 0;
-            add.add(b+c);
-        }
-
-        return add;
     }
 }
